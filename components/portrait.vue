9c5da861b5117f6e74da99eb6079715286ed092d
James MacIvor <james.macivor1@gmail.com>
2020-02-18 07:12:08 -0500

    finished home page layout

--- components/portrait.vue
+++ components/portrait.vue
@@ -1,12 +1,5 @@
 <template>
     <div class="flex justify-center mb-4">
-        <img src="../assets/images/jamesportrait.webp" alt="a picture of james macivor - completely on brand with crooked glasses, bad haircut, and beard stubble."/>
+        <img class="w w-3/4 md:w-40 rounded-lg" src="../assets/images/jamesportrait.webp" alt="a picture of james macivor - completely on brand with crooked glasses, bad haircut, and beard stubble, smirking, looks kinda ridiculous."/>
     </div>
 </template>
-
-<style scoped>
-img {
-    width: 25%;
-    border-radius: 1rem;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="container">
-    <div>
+  <div class="">
+    <div class="h-10 w-full bg-indigo-700"></div>
+    <div class="container-title bg-indigo-700 flex justify-center">
+        <h1 class="font-semibold text-5xl md:text-4xl mb-5">
+          Hi, I'm James MacIvor
+        </h1>
+    </div>
+    <div class="container-center mt-8">      
       <portrait />
-      <h1 class="title">
-        James MacIvor
-      </h1>
-      <h2 class="subtitle">
-        My Personal Site
+      <h2 class="subtitle font-medium text-3xl mb-5 p-2">
+        Welcome to My Website
       </h2>
-      <div class="links">
-        <a class="button--green">My Career</a>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          My Blog
-        </a>
-        <a class="button--green">
-          My Company
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--green"
-        >
-          About Me
-        </a>
+      <div class="links w-full">
+          <a class="button--green">Career</a>
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green"
+          >
+            Blog
+          </a>
+          <a class="button--green">
+            Company
+          </a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--green"
+          >
+            About Me  
+          </a>
       </div>
     </div>
   </div>
@@ -42,40 +45,38 @@ export default {
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+<style lang="postcss">
+.container-title {
+  background: linear-gradient(90deg, #4c51bf, #313587);
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.container-title > h1 {
+  color: white;
+}
+.links a {
+  @apply block w-full font-bold text-xl border border-solid border-indigo-500;
+  border-bottom: none;
+  padding: 5%;  
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+@screen md {
+  .links {
+    @apply flex;    
+  }
+  .links a {
+    @apply w-1/4 border border-solid border-indigo-500
+  }
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+@screen lg {
+  .links a {
+    @apply w-40 text-center text-base p-0 pt-3 pb-3;
+  }
+  .links {
+    @apply justify-center;
+  }
 }
 
-.links {
-  padding-top: 15px;
+.links a:hover, a:active {
+  @apply bg-indigo-700 text-white
 }
 </style>
