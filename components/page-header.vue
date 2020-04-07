0043a10279287b22c8331a394286eb935aaa596d
James MacIvor <james.macivor1@gmail.com>
2020-04-07 09:37:28 -0400

    updated page-header to use updated css

--- components/page-header.vue
+++ components/page-header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>        
         <div class="container-title bg-indigo-700 flex justify-center">
-            <h1 class="font-semibold text-5xl md:text-4xl m-5">{{title}}</h1>
+            <h1>{{title}}</h1>
         </div>
     </div>
 </template>
