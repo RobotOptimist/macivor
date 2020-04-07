ac50536fec0c51f23cd8c2a92c150c8ecf94691e
James MacIvor <james.macivor1@gmail.com>
2020-04-07 09:37:55 -0400

    updated copy on about me

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,19 @@
 <template>
-    <page-header title="About Me" />
+    <section>        
+        <page-header title="About Me" />
+        <article class="article-container">
+            <div class="container-center">
+                <div class="text-center">
+                    <h2>I'm not a serious person</h2>
+                </div>
+                <p>I love working, but I'm not my job. I like a whole bunch of things - most of all I love hanging out with my family. </p>
+                <p>I play games, like table top roleplaying games and video games.</p> 
+                <p>I get political from time to time. I'm love science fiction and fantasy novels, shows and movies.</p> 
+                <p>Coding is fun, and I'm glad to do it as a hobby as well as my job - but I do a bunch of different stuff. It's important not to be one sided, yeah? </p>
+                <p>The common thread is I love whimsy and doing things for the joy of doing it.</p>
+            </div>
+        </article>
+    </section>
 </template>
 
 <script>
