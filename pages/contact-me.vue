b629850cb630e19f380f270a50e614674b57a6df
James MacIvor <james.macivor1@gmail.com>
2020-08-20 16:35:30 -0400

    changed contact me form to not redirect

--- pages/contact-me.vue
+++ pages/contact-me.vue
@@ -1,31 +1,76 @@
 <template>
-    <section>
-        <page-header title="Contact James"></page-header>
-        <div class="container-center">
-            <p class="mt-12">What's on your mind?</p>
-            <form class="flex flex-col mt-12 p-4" action="https://formspree.io/myyddpzn" method="post">
-                <label class="mt-4" for="email">
-                    Your Email Address
-                    <input class="p-2 shadow-md rounded-lg w-full mt-2" id="email" type="email" name="email">
-                </label>
-                <label class="mt-4" for="message">
-                    Your Message
-                    <textarea class="p2 shadow-md rounded-lg w-full mt-2 h-24" id="message" name="message"></textarea>
-                </label>
-                <div>
-                    <button class="px-4 py-2 text-indigo-200 bg-indigo-700 hover:text-white mt-4 rounded-lg" type="submit">Submit*</button>
-                </div>                
-            </form>
-            <p class="text-sm mt-4">*You'll be redirected to Formspree on a successful submission. Yeah I could send this form with JavaScript and avoid that but, *sigh* - meh.</p>
+  <section>
+    <page-header title="Contact James"></page-header>
+    <div class="container-center">
+      <p class="mt-12">What's on your mind?</p>
+      <form class="flex flex-col mt-12 p-4" @submit.prevent="submit">
+        <label class="mt-4" for="email">
+          Your Email Address
+          <input
+            class="p-2 shadow-md rounded-lg w-full mt-2"
+            id="email"
+            type="email"
+            name="email"
+            v-model="data.emailAddress"
+          />
+        </label>
+        <label class="mt-4" for="message">
+          Your Message
+          <textarea
+            class="p-2 shadow-md rounded-lg w-full mt-2 h-24"
+            id="message"
+            name="message"
+            v-model="data.message"
+          ></textarea>
+        </label>
+        <div>
+          <button
+            class="px-4 py-2 text-indigo-200 bg-indigo-700 hover:text-white mt-4 rounded-lg"
+            type="submit"
+          >Submit</button>
         </div>
-    </section>
+      </form>
+      <div v-if="response != null">
+        <div v-if="success">
+          <p>Thanks! Feel free to email me again if you have any other thoughts.</p>
+        </div>
+        <div v-else>
+          <p>Oops, some kinda problem here, idunno.</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import pageHeader from "~/components/page-header";
+import pageHeader from '~/components/page-header'
 export default {
-    components: {
-        pageHeader,
+  components: {
+    pageHeader,
+  },
+  data() {
+    return {
+      data: {
+        emailAddress: '',
+        message: '',
+      },
+      response: null,
+      success: true,
     }
+  },
+  methods: {
+    async submit(e) {
+      e.preventDefault();  
+      this.response = await fetch('https://formspree.io/myyddpzn', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.data),
+      })
+      this.data.message = ''
+    },
+  },
 }
 </script>
