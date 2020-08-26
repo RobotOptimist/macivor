ef999a9a0c92a1dabaf2eba176ea162bf42e79df
James MacIvor <james.macivor1@gmail.com>
2020-08-26 10:39:56 -0400

    added memes and proofread article

--- pages/blog/the-cure-for-imposter-syndrome.vue
+++ pages/blog/the-cure-for-imposter-syndrome.vue
@@ -1,7 +1,7 @@
 <template lang="md">
     <page-header title="The Cure For Imposter Syndrome" />
 
-    ::: .container-center .mt-8
+    ::: .container-center
 
     ## We Suffer From Our Comparisons
     
@@ -17,91 +17,107 @@
 
     ::: .article-container
 
-    ### What am I talking about?
-
     * Every single person I know has had it. 
     * It can keep you awake at night. 
     * It makes you timid when you should be bold. 
     * It's the voice that convinces you to take the easy path. 
-    * Don't take risks. 
-    * Don't challenge the senior developer.
+        * Don't take risks. 
+        * Don't challenge the senior developer.
+        * Don't go for that promotion.            
 
     Of course, I'm talking about Imposter Syndrome.
+
+    ### What Even Is Imposter Syndrome?
     
     Imposter Syndrome runs rampant in our industry, preying on our fragile mental health. It's likely most frequently seen with junior developers, but I've seen it with mid levels and senior developers as well.
 
-    And, in a good team, it all shakes out. There is a support structure of people who will reassure you that you're doing a good job. Or will make themselves a little bit vulnerable to show you that you're not alone. This is good healthy communication.
+    And, in a good team, it can be somewhat mitigated. There is a support structure of people who will reassure you that you're doing a good job. Or will make themselves a little bit vulnerable to show you that you're not alone. This is good healthy communication.
 
     But not all teams are like that. In fact, that might be a bit of an exception. Too often, there are teams where people feel no obligation to reassure you, or worse they compensate for their own feelings of inadequacy by exacerbating yours.
 
-    (And if you are on a team with such a bully and can't leave, continue reading for coping mechanisms.) 
+    <div class="flex justify-center">        
+        <picture-wrapper 
+            fileExtension="jpg" 
+            fileName="kick-em-when-their-down" 
+            altText="Meme cartoon where three characters labeled as self doubt, social anxiety and feeling like a fraud bully a nameless fourth character. A fifth character labeled as Your Team offers a helping hand only to suplex the bullied character. Text at the bottom reads An Unsympathetic Team Can Make Things So Much Worse"
+        >
+        </picture-wrapper>
+    </div>
 
     So, the working definition for Imposter Syndrome is this (from Wikipedia):
 
     > Imposter Syndrome is a psychological pattern in which an individual doubts their accomplishments and has a persistent internalized fear of being exposed as a "fraud".
 
-    Worse, you'll find your self thinking about the "Peter Principle" and wondering, "Am I Peter?"
+    And here is the rub - the root of Imposter Syndrome is disbelief in your own success. How does that happen? You're accomplishments are right there in the ol' resume. You can track through a long list of completed user stories. You can see old PRs of your contributions - and yet no matter what evidence is presented this feeling persists.
 
-    The Peter Principle definition (from Wikipedia): 
+    This is a defining symptom of Imposter Syndrome. the psychological journal [verywellmind](https://www.verywellmind.com/) has an [article](https://www.verywellmind.com/imposter-syndrome-and-social-anxiety-disorder-4156469) that has the following bulleted list of characteristics:
 
-    > The Peter principle is a concept in management developed by Laurence J. Peter, which observes that people in a hierarchy tend to rise to their "level of incompetence": employees are promoted based on their success in previous jobs until they reach a level at which they are no longer competent, as skills in one job do not necessarily translate to another. 
+    * Self-doubt
+    * **An inability to realistically assess your competence and skills**
+    * Attributing your success to external factors
+    * Berating your performance
+    * Fear that you won't live up to expectations
+    * Overachieving
+    * Sabotaging your own success
+    * Setting very challenging goals and feeling disappointed when you fall short
 
-    In other words, people get promoted up to where they are ineffective or incompetent and then they're stuck.
+    Do any of these sound familiar? The fact is that we all experience some of this throughout our careers, but composed together we start to feel as though we are frauds. 
 
-    ### What Causes It? 
+    ### What Causes It?
 
-    And here is the rub - the cause of Imposter Syndrome is disbelief in your own success. How does that happen? You're accomplishments are right there in the ol' resume. You can track through a long list of completed user stories. You can see old PRs of your contributions - and yet no matter what evidence is presented this feeling persists.
-
-    It comes from comparing yourself to others, not how they are but how you imagine them to be. There are so many tremendously smart people in tech, it's tempting to see them as giants. Like they, through 100% of their own merit, were given opportunities to work on new technologies and present themselves to the world. Even though many of them would be the first to deny this alt-version of themselves! People like Scott Hanselman are quick to remind others that he once stood where you are.
+    It comes from comparing yourself to others, not how they are but how you imagine them to be. There are so many tremendously smart people in tech, it's tempting to see them as giants. Like they, through 100% of their own merit, were given opportunities to work on new technologies and present themselves to the world. Even though many of them would be the first to deny this alt-version of themselves! People like [Scott Hanselman are quick to remind others that he once stood where you are](https://www.hanselman.com/blog/ImAPhonyAreYou.aspx). Moreover, Dan Abramov directly addresses reality vs. hype for developer expertise in his [blog post](https://overreacted.io/things-i-dont-know-as-of-2018/).
 
     More common, but less obvious, is when you compare ourself to people whom you think you should have parity and are shocked when they have deep knowledge of something unfamiliar to you. This happens to all of us. How quick we are to scold ourselves for not making the same decisions, studying the same things, making the same sacrifices. We're not fair to ourselves.
 
     And yet, it's useless to talk ourselves out of these feelings. It's a rare day when I can negotiate with my feelings regardless! I just hide the negative ones, same as you I expect. 
 
+    In the Time article ["How to Deal With Imposter Syndrome"](https://time.com/5312483/how-to-deal-with-impostor-syndrome/#:~:text=How%20to%20deal%20with%20impostor,more%20critically%20question%20those%20thoughts.) Psychologist Audrey Ervine says
+    
+    > There’s no single answer. Some experts believe it has to do with personality traits—like anxiety or neuroticism—while others focus on family or behavioral causes, Sometimes childhood memories, such as feeling that your grades were never good enough for your parents or that your siblings outshone you in certain areas, can leave a lasting impact.
+
+    The verywellmind article further explains the causes of Imposter Syndrome.
+
+    >  We also know that entering a new role can trigger impostor syndrome. For example, starting college or university might leave you feeling as though you don't belong and are not capable.
+
+    Moreover, we learn that Social Anxiety Disorder and Imposter Syndrom overlap, which makes a lot of sense when you consider that Imposter Syndrome results in a great deal of anxiety regarding how we're perceived by others.
+
     ### Wait, You Promised A Cure?
 
     Yes, I did.
 
-    I'm sorry to say, but the "cure" takes a while. It comes in the form of constant affirmation.
+    I'm sorry to say, but the "cure" takes a while. It comes in the form of constant affirmation. And, if you believe that you're experiencing this in conjunction with Social Anxiety Disorder, then additional steps might be needed.
 
-    I achieved this by becoming much more serious about mentoring. Teaching people how to code and mentoring junior developers helps me see how far I've come. 
+    <div class="flex justify-center my-4">        
+        <picture-wrapper             
+            fileName="alter-deal" 
+            altText="Darth Vader stating to Boba Fett 'I have altered the deal, pray I do not alter it further'"            
+        >
+        </picture-wrapper>
+    </div>
 
-    I strongly recommend if you're a junior developer - try teaching a curious friend the basics of coding. If your a mid level or senior then commit yourself to mentoring a junior developer. What's great about this is it shows you how far you've come when you're showing someone how to write a simple program - you know the stuff it took you months to learn. 
+    The first thing you can do that really helps is talk to someone. This is why being on a team that offers that kind of support is important. If you feel safe talking to someone on your team, then much of this can be dispelled. Don't try to solo it! It's important to be honest with yourself and others.
+
+    Another thing you can do is to focus on other people who might be going through the same thing. I achieved this by becoming much more serious about mentoring. Teaching people how to code and mentoring junior developers helps me see how far I've come. At the same time, I can see where they might feel insecure and it's a good opportunity to listen and reassure them.
+
+    I strongly recommend if you're a junior developer - try teaching a curious friend the basics of coding. If your a mid level or senior then commit yourself to mentoring a junior developer. It can be a great reminder of the learning and leaps you've made since you began this journey when someone else is looking up to you. This is a good way to constructively assess your abilities.
+
+    I also recommend seeking out those stronger developers as mentors for yourself. Put yourself in a position to listen to them rather than compete with them. It's a joy to work with super competent people who care about their work and their team.
     
-    We are constantly comparing ourselves to others. It can't be helped! It's how our brains are wired, and if you only ever compare yourself to the exceptional people then you'll fail to see your own progress. Even better though, as you continue to teach and mentor you'll see the people you boosted begin to surpass you. This is a good thing! This means you were an EXCELLENT mentor. Be proud.
+    We are constantly comparing ourselves to others. It can't be helped! It's how our brains are wired, but if that's all you do then you'll fail to see your own progress. Even better though, as you continue to teach and mentor you'll see the people you boosted begin to surpass you. This is a good thing! This means you were an EXCELLENT mentor. Be proud. It's also great to have a mentor rooting for you, invested in your success - and ready to dispel any notion that you're a fraud.
 
-    Is it selfish to teach and mentor to make yourself feel better? No? Yes? I don't know - I don't think it matters. If you feel bad about it then try to forgive yourself and do it anyway - or replace the reason for doing it with one of the many valid selfless reasons people do this. Then you can look at the boost to your own confidence as a side benefit 😁.
+    It's natural and right to use mentoring and teaching to improve ourselves and the people around us. It breaks down those social walls that hold us apart and creates a community.
 
-    ### And The Bullies?
-
-    Right, they're out there - ready to capitalize on your self-doubt. If you can't avoid them then you've got to deal with them.
-
-    The first thing to do is to recognize them. Someone who provides genuine constructive criticism is way different than a bully. But it can be hard to tell sometimes if the genuine person is maybe just bad at communicating. So lets look at some things to make this determination.
-
-    1. Was it really feedback? Feedback has details. Feedback suggests a way to improve. Saying "wooowww that is some *special* code" does none of those things. People might say "I think it can be improved" and they'll usually be able to provide somekind of detail. People who can't or won't are not interested in making a good product - they just want to dunk on you.
-    2. Was it sarcastic? Sarcasm is a poison pill for making any kind of point - and I say that as someone who engages in sarcasm from time to time myself! A solid criticism delivered sarcastically is no different from an insult. 
-    3. Was it condescending? This is a bad one in software circles. I'm ashamed to say I've done it too. If someone really believes they are *that much better* then it might not be worth engaging with them.
-
-    There are many other forms of bullying: talking over you, yelling at you, direct insults. That's the more overt stuff. What I usually see is more subtle and can be called a "neg" which is a way to manipulate you by making you doubt yourself.
-
-    It takes effort to deal with someone like this - but it's necessary. You'll need to practice a little bit of personal courage in order to engage.
-
-    First, set limits. Tell them to stop. It can feel like you're overreacting but don't worry about that. Call out the bad behavior for what it is, sarcasm, condescension, etc. Be specific about the behavior that's bugging you.
-
-    Second, let them know why it's bad - for example "I can't get meaningful, actionable feedback from what you said because it either doesn't have any actionable content, or it's drenched in sarcasm".
-
-    Third, instruct them on how they should proceed going forward. Like "I appreciate feedback in this format - state the problem and then suggest a solution, or provide reasonable suggestions for additional research".
-
-    The goal here is not to "win". There is no way to win an interaction like this - so don't approach it like a contest of wills or anything. Just state the facts as you see them. In fact, for many of these interactions there need not be any animosity either. The less energy this costs you, the better.
-
-    How can this energy expenditure be avoided though? Silence. Silence is your friend. When you provide a calm actionable statement and the other party becomes upset, keep your face blank, maintain eye contact and say nothing. Just wait. I'm serious! This really works! 
-
-    By keeping yourself silent you force them to hear themselves - an angry statement just sort of echoes around for a bit. They'll hear it and start imagining what you're counter argument will be. When you fail to supply it then sometimes they'll do it for you and then they're arguing with themselves. Remain silent even then, eventually they'll disengage or they'll try to match you in being calm. Either way, you delivered your point and can move on.
-
-    This trick is something I learned through many years of customer service - in person and on the phone.
+    <div class="flex justify-center my-4">        
+        <picture-wrapper 
+            fileExtension="jpg" 
+            fileName="expanding-brain-meme" 
+            altText="Expanding brain meme, little brain is feeling inadequate in your job, larger brain is combating your own imposter syndrome, next largest brain is realizing you've been competent all along, enlightened brain is dismantling structural problems in your job that exist to make people feel incompetent"            
+        >
+        </picture-wrapper>
+    </div>
     
-    Try to build this into your affirmation as well. You dealt with unnessarily negative feedback as a true professional. Pat yourself on the back. It's not easy. But also understand that some of that Imposter Syndrome came from an external source. By dealing with it directly you will improve your self-confidence.
-
+    Like with anything of this nature, there are no magic pills to make those feelings of inadequacy go away. We've got to do the work of finding our place and accepting ourselves as we are.
+ 
     :::
 
     :::
