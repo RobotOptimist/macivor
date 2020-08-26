59dd734418776294aebfc1c4a4fb526d8fc425d0
James MacIvor <james.macivor1@gmail.com>
2020-08-26 10:51:13 -0400

    fixed some styling and added title and description

--- pages/blog/the-cure-for-imposter-syndrome.vue
+++ pages/blog/the-cure-for-imposter-syndrome.vue
@@ -132,5 +132,17 @@ export default {
     pageHeader,
     pictureWrapper,
   },
+  head() {
+    return {
+      title: 'Blog: The Cure For Imposter Syndrome',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: "A description of Imposter Syndrome and how to overcome it.",
+        },
+      ],
+    }
+  },
 }
 </script>
