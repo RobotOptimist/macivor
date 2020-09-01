67809af30cf5e788bdef6a9f0892e47158c7a9fa
James MacIvor <james.macivor1@gmail.com>
2020-09-01 10:09:40 -0400

    fixed issue with wrong url in feed

--- pages/rss/_blog.vue
+++ pages/rss/_blog.vue
@@ -6,6 +6,18 @@
 
 <script>
 export default {
+  head() {
+    return {
+      title: this.page.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.page.description
+        },
+      ],
+    }
+  },
   async asyncData({ $content, params }) {
         const page = await $content(`blog/${params.blog}`).fetch()
         return {
